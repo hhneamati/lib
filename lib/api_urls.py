0df81b943900f4dbdc9134f9ba2cc82d08fbd9a5
hadi <h.hneamati@gmail.com>
2021-07-10 06:26:47 +0430

    gitifnor comit

--- lib/api_urls.py
+++ lib/api_urls.py
@@ -7,3 +7,5 @@ router = routers.DefaultRouter()
 router.register('Book' , BookViewset , basename='book')
 router.register('publisher' , PublisherViewset , basename='publisher')
 router.register('author' , AuthorViewset , basename='author')
+
+
